d62922263083fe5fd59862ea1204d68beeae927f
steven yu <jpstevenjpyu@gmail.com>
2024-06-27 21:22:28 -0700

    added test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,3 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+test('renders the App component', () => {
+  render(<App />);
+  expect(screen.getByText(/stevenyucodes portfolio/i)).toBeInTheDocument();
+});
