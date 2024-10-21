e39e5d4254bb181a9ccb0a3b906d2674b0c9b9ab
stevenyucodes <stevenyucodes@users.noreply.github.com>
2024-10-21 01:46:43 +0000

    deploy: f22ced9926e07287ac906fdd7d9d6a1aa914c421

---
+++ precache-manifest.5f252ca41a1010d094313796c76c8680.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da06e92b1b9db439a12791e16b00b9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bed443eeca66991d2f9",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7c8dde6044d34ee205fb",
+    "url": "/static/js/2.6d4522b7.chunk.js"
+  },
+  {
+    "revision": "b1e0979a16f2e1974518",
+    "url": "/static/js/3.e26ec9e0.chunk.js"
+  },
+  {
+    "revision": "9bed443eeca66991d2f9",
+    "url": "/static/js/main.296c286c.chunk.js"
+  },
+  {
+    "revision": "dc3c7efc69846c32227c",
+    "url": "/static/js/runtime~main.9b1c6bb6.js"
+  }
+]);
